9bbf5be0e6ededc4a5654dbe2478c9de287fb6de
ShristiC7 <148475072+ShristiC7@users.noreply.github.com>
2024-09-08 19:36:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+for(var n=0; n<document.querySelectorAll(".drum").length; n++){
+    document.querySelectorAll("button")[n].addEventListener("click", function(){
+        var btninnerHtml = this.innerHTML;
+        audioadd(btninnerHtml);
+    });
+    document.addEventListener("keydown", function(event){
+        audioadd(event.key);
+    });
+}
+
+function audioadd(key){
+    switch(key){
+        case "w":
+        var audio1=new Audio('./sounds/tom-1.mp3');
+    audio1.play();
+    break;
+    case "a":
+        var audio2=new Audio('./sounds/tom-2.mp3');
+    audio2.play();
+    break;
+    case "s":
+        var audio3=new Audio('./sounds/tom-3.mp3');
+    audio3.play();
+    break;
+    case "d":
+        var audio4=new Audio('./sounds/tom-4.mp3');
+    audio4.play();
+    break;
+    case "j":
+        var audio5=new Audio('./sounds/crash.mp3');
+    audio5.play();
+    break;
+    case "k":
+        var audio6=new Audio('./sounds/kick-bass.mp3');
+    audio6.play();
+    break;
+    case "l":
+        var audio7=new Audio('./sounds/snare.mp3');
+    audio7.play();
+    default: console.log(key);
+    }
+}
